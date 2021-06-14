e7193134ad2f086f760af9f7930dd07bd421027d
Vijayant <vy369b@gmail.com>
2021-06-14 18:00:54 +0530

    Modified allowed host

--- mywork/settings.py
+++ mywork/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'vijayadav.pythonanywhere.com']
 
 
 # Application definition
