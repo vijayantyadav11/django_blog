b72a4bba9d24192b9d76d59b0b507b9cc25417df
Vijayant <vy369b@gmail.com>
2021-06-14 19:31:06 +0530

    Modified debug, allowed host

--- mywork/settings.py
+++ mywork/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c42+53w9u*%*v6t-+&sf)x4b*)o)9(9y=*c-kau$pvo&9*44b6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'vijayvns.pythonanywhere.com']
 
